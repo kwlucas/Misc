a169239d0b8e881883c9a6024517d53252c62e2b
kwlucas <lucaswkyle@gmail.com>
2022-10-09 16:35:23 -0600

    Psudo code

---
+++ python/hwHelpElucas1.py
@@ -0,0 +1,23 @@
+#HEADER
+print("Welcome")
+print("Program and Program Number")
+print("Description")
+print("------------------------")
+
+#Get user input for number 1
+#Get user input for number 2
+#Get user input for number 3
+#Get user input for number 4
+#Get user input for number 5
+
+#Output number 1
+#Output number 2
+#Output number 3
+#Output number 4
+#Output number 5
+#Add numbers together
+#Output the result of numbers added together
+#Average the numbers 
+#Output the result of acerage of numbers
+
+
