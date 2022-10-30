4769f0723d6cb7b81ee4f37c3e493c3fe9f1de6a
kwlucas <lucaswkyle@gmail.com>
2022-10-30 10:44:39 -0600

    Psudeo code

---
+++ python/hwHelpElucas2.py
@@ -0,0 +1,38 @@
+#Header 1- name assignmentNumber ProgramNumber Description
+#Loop if int is not "quit"
+#Ask for int
+#Check if negative, zero, or positive
+#Tell of negative, zero, or positive
+
+#Header 2- name assignmentNumber ProgramNumber Description
+#Loop if ammount is not quit
+#Ask for ammount
+#check if over 10,000
+    #Tell if over 10,000
+    #Loop again if yes
+#divide by 20
+#round down to nearest factor
+#subtract the 20s from total value
+#divide by 10
+#round to nearest factor
+#subtract the 10s from total value
+#repeat for 5s and 1s, coins
+
+#Header 3- name assignmentNumber ProgramNumber Description
+#Loop if string is not quit
+#Ask for string
+#check if string is only letters
+#check if string is only uppercase
+#check if string is only lowercase
+#check if string is only digits
+#check if string is alphanumeric
+#check if string starts with a capital
+#check if string ends with a period
+
+#Header 4- name assignmentNumber ProgramNumber Description
+#Loop of income is not quit
+#ask for income
+#Check which bracket income is
+#Determine % accordingly
+#Calculate tax ammount using %
+#Output tax ammount
