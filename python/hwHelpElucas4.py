2f1cab836bfc4bc6e1abaef6fe5b530aca83cd8f
kwlucas <lucaswkyle@gmail.com>
2022-11-05 12:03:50 -0600

    Created program 3 on hw 4

--- python/hwHelpElucas4.py
+++ python/hwHelpElucas4.py
@@ -39,3 +39,41 @@ num2 = float(input('Enter the height: '))
 print(findArea(num1, num2))
 
 print()
+print()
+
+def translatePhoneNum(phoneNumber):
+    key = {
+        'a': '2',
+        'b': '2',
+        'c': '2',
+        'd': '3',
+        'e': '3',
+        'f': '3',
+        'g': '4',
+        'h': '4',
+        'i': '4',
+        'j': '5',
+        'k': '5',
+        'l': '5',
+        'm': '6',
+        'n': '6',
+        'o': '6',
+        'p': '7',
+        'q': '7',
+        'r': '7',
+        's': '7',
+        't': '8',
+        'u': '8',
+        'v': '8',
+        'w': '9',
+        'x': '9',
+        'y': '9',
+        'z': '9',
+    }
+    phoneNumber = phoneNumber.lower()
+    translateTable = phoneNumber.maketrans(key)
+    translated = phoneNumber.translate(translateTable)
+    return translated
+
+phoneNum = input("Enter a phone number with text in it. (Dont include hyphens): ")
+print(translatePhoneNum(phoneNum))
